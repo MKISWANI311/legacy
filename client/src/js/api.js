c997d6dd273d6f119c03e4192f9df92b04c49fd3
Stanislav Kalashnik <darkpark.main@gmail.com>
2019-04-25 23:51:05 +0300

    revert CORS (credentials are required because of cookies)

--- client/src/js/api.js
+++ client/src/js/api.js
@@ -8,7 +8,7 @@
 var defaults = {
     server: localStorage.getItem('server') || 'https://api.fortnotes.com/',
     mode: 'cors',
-    //credentials: 'include',
+    credentials: 'include',
     headers: {
         Accept: 'application/json, text/plain, */*',
         'Content-Type': 'application/json'
